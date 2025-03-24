35db7e37a6090fd83b634e5edb77476aa7d2d87a
Lebedev Roman <balakovo_roman@mail.ru>
2025-03-24 20:56:40 +0300

    Added cmake make static library function

---
+++ cmake/make-static.cmake
@@ -0,0 +1,5 @@
+function(make_static_lib target_name)
+    add_library(${target_name} STATIC)
+    target_sources(${target_name} PRIVATE ${ARGN})
+    add_library(static::${target_name} ALIAS ${target_name})
+endfunction()
